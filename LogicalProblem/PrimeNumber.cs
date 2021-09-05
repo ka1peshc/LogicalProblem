4de45edb1e741f36b2dc37464a927beb3987d34a
ka1peshc <kalpesh111997@gmail.com>
2021-09-05 08:30:36 +0530

    Checking if given number is divisible by any other than  1 and itself

---
+++ LogicalProblem/PrimeNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProblem
+{
+    class PrimeNumber
+    {
+        internal void checkPrimeNumber(int num)
+        {
+            int check = 0;
+            for(int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    check = 1;
+                    break;
+                }
+            }
+            if (check == 1)
+            {
+                Console.WriteLine(num + " is not a prime number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is a prime number");
+            }
+        }
+    }
+}
--- LogicalProblem/Program.cs
+++ LogicalProblem/Program.cs
@@ -11,11 +11,17 @@ namespace LogicalProblem
             Console.WriteLine("Enter number till fibonacci number series");
             int n = Convert.ToInt32(Console.ReadLine());
             fi.fibonacciSeries(n);
-            */
+            
             PerfectNumber pf = new PerfectNumber();
             Console.WriteLine("Enter number to check if it is Perfect number");
             int num = Convert.ToInt32(Console.ReadLine());
             pf.checkPerfectNumber(num);
+            */
+
+            PrimeNumber pn = new PrimeNumber();
+            Console.WriteLine("Enter number to check if it is Prime number");
+            int numPrime= Convert.ToInt32(Console.ReadLine());
+            pn.checkPrimeNumber(numPrime);
         }
     }
 }
