ce4b0fdb7321360fa5d287bbabe1d3277b7d9c49
ka1peshc <kalpesh111997@gmail.com>
2021-09-05 08:10:23 +0530

    Checking if sum of divisible of n is equal to given n

---
+++ LogicalProblem/PerfectNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProblem
+{
+    class PerfectNumber
+    {
+        internal void checkPerfectNumber(int n)
+        {
+            String Divisor="1";
+            int sum = 1;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Divisor = Divisor+"+"+i.ToString();
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+            if (sum == n)
+            {
+                Console.WriteLine("{0} is perfect number",n);
+                Console.WriteLine(Divisor);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not perfect number", n);
+                Console.WriteLine(Divisor);
+            }
+        }
+    }
+}
--- LogicalProblem/Program.cs
+++ LogicalProblem/Program.cs
@@ -6,10 +6,16 @@ namespace LogicalProblem
     {
         static void Main(string[] args)
         {
+            /*
             Fibonacci fi = new Fibonacci();
             Console.WriteLine("Enter number till fibonacci number series");
             int n = Convert.ToInt32(Console.ReadLine());
             fi.fibonacciSeries(n);
+            */
+            PerfectNumber pf = new PerfectNumber();
+            Console.WriteLine("Enter number to check if it is Perfect number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            pf.checkPerfectNumber(num);
         }
     }
 }
